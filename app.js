74cec0e80b8e623042aed4d9c1079da2fe5bccb3
Eleanor Atkinson <atkinson.el@northeastern.edu>
2023-12-05 17:09:52 -0500

    edit to app.js

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
+
 import UserRoutes from "./users/routes.js";
 const app = express();
 app.use(
