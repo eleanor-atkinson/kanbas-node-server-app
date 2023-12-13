7630b1debabadd9f3ce71d40824ce37fa81239ee
Eleanor Atkinson <atkinson.el@northeastern.edu>
2023-12-13 09:33:49 -0500

    a6 local fix

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
 import mongoose from "mongoose";
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect('mongodb://127.0.0.1:27017/kanbas');
 
-import UserRoutes from "./users/routes.js";
 const app = express();
 app.use(
     cors({
@@ -39,4 +39,5 @@ ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+
 app.listen(process.env.PORT || 4000);
